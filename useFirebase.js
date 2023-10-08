eb57dfba5b0475d0c014bdaa05aa038673c6955f
NavyGhost1942 <riverawilliam256@gmail.com>
2023-10-08 00:58:09 -0500

    update

---
+++ useFirebase.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCM8s7dMmcwH9AeTD-tUXc73KIutfunlt0",
+
+  authDomain: "vue-3-company-directory-464b9.firebaseapp.com",
+
+  projectId: "vue-3-company-directory-464b9",
+
+  storageBucket: "vue-3-company-directory-464b9.appspot.com",
+
+  messagingSenderId: "341582314883",
+
+  appId: "1:341582314883:web:3545e48787521bac310ab6"
+
+};
+
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
